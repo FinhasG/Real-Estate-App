5b19510a2d367d3a9551a71b02d52b18eb328f3f
Finhas Ghebreyonas <fanoylove7@gmail.com>
2023-11-07 00:08:29 +0300

    Connect Server to Database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express=require("express");
 const app=express();
+require('dotenv').config();
+const connectDB=require('./database/db')
 
-app.listen(4000,(req,res)=>{
-    console.log("successfully connected")
-})
+
+
+const server=async()=>{
+    await connectDB(process.env.MONGO_URL)
+    app.listen(4000,(req,res)=>{
+        console.log("successfully connected")
+    })
+}
+
+server()
+
