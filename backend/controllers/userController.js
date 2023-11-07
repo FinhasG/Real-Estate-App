c1ce7458922d2ba6dde9d74cc834253de3a931e7
Finhas Ghebreyonas <fanoylove7@gmail.com>
2023-11-07 15:16:04 +0300

    Create a test api route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+
+
+const test=async(req,res)=>{
+    res.json({msg:"Working ok"})
+}
+
+module.exports={test}
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express=require("express");
 const app=express();
 require('dotenv').config();
 const connectDB=require('./database/db')
+const userRoutes=require('./routes/user')
+
+app.use(express.json());
+
+
+
+app.use('/api/user',userRoutes)
 
 
 
